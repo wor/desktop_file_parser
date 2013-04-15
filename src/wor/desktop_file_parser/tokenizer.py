f0fa99533828d7be00f5a4ecc449af76669b4b33
Esa Määttä <wor@iki.fi>
2013-04-15 20:18:06 +0300

    tokenizer: rework token class hierarchy
    KISS. Also now comment lines are recognized.

--- src/wor/desktop_file_parser/tokenizer.py
+++ src/wor/desktop_file_parser/tokenizer.py
@@ -2,8 +2,8 @@
 """Desktop file tokenizer."""
 
 import types
-import wor.tokenizer as tok
 import re
+import wor.tokenizer as tok
 
 # What is needed to use tok.Token from Symbol
 
@@ -32,36 +32,44 @@ SYMBOL_CLASS_NAMES = [
 "URL",
 ]
 
-class Symbol(tok.Token):
-    def __init__(self, value=None, pos=None, subvalues=[]):
-        super().__init__(pos, value, subvalues)
+#
+# Token classes
+#
 
-class GroupHeader(Symbol):
+class GroupHeader_T(tok.Token):
     """Token class for desktop entry file group headers."""
+    pass
+
+class EmptyLine_T(tok.Token):
+    pass
+
+class CommentLine_T(tok.Token):
+    pass
+
+class Entry_T(tok.Token):
     pass
 
 
 def init_tokenizer():
     """Initializes desktop file tokenizer and returns it.
-    """
-    # Create Symbols
-    symbol_classes = {}
-    for symbol_class_name in SYMBOL_CLASS_NAMES:
-        symbol_classes[symbol_class_name] = types.new_class(symbol_class_name, (Symbol,))
 
+    Returns:
+        wor.tokenizer.Tokenizer. Initialized instance of Tokenizer class.
+    """
     # Create symbol table
     symbol_table = tok.TokenTable("Main")
-    symbol_table.default_token_class = Symbol
+    symbol_table.default_token_class = tok.Token
 
     # Add symbols to the symbol table
     # The symbol regexp are interpreted as multiline regex when tokenizer joins
     # them.
     ns = symbol_table.add_new_token
     for symbol_name in SYMBOL_CLASS_NAMES:
-        ns(symbol_name.upper().replace('-','_'), r"^" + symbol_name + r"(\[.+\])?=(.*)$\n?", token_subclass=symbol_classes[symbol_name])
+        ns(symbol_name.upper().replace('-','_'), r"^(" + symbol_name + r")(\[.+\])?=(.*)$\n?", token_subclass=Entry_T)
 
-    ns("EMPTY_LINE", r"^\s*\n", token_subclass=Symbol)
-    ns("GROUP_HEADER", r"^\[(.+)\]\s*$\n?", token_subclass=GroupHeader)
+    ns("COMMENT_LINE", r"^#(.*)\n", token_subclass=CommentLine_T)
+    ns("EMPTY_LINE", r"^\s*\n", token_subclass=EmptyLine_T)
+    ns("GROUP_HEADER", r"^\[(.+)\]\s*$\n?", token_subclass=GroupHeader_T)
 
     # Create tokenizer with defined symbol table
     try:
